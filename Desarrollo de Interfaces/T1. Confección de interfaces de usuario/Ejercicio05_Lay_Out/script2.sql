474d72572360aba094b4f17aee42045a27e866f4
unknown <movilsergio2000@gmail.com>
2019-11-22 17:47:22 +0100

    Ejercicio de Desarrollo de Interfaces

--- Desarrollo de Interfaces/T1. Confección de interfaces de usuario/Ejercicio05_Lay_Out/script2.sql
+++ Desarrollo de Interfaces/T1. Confección de interfaces de usuario/Ejercicio05_Lay_Out/script2.sql
@@ -1,5 +1,5 @@
 use desarrollo
-CREATE TABLE [dbo].[Formularios2](
+CREATE TABLE [dbo].[Textos](
 	idTexto int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	campo [varchar](50) NOT NULL,
 	valor [varchar](100) NOT NULL,
@@ -7,45 +7,45 @@ CREATE TABLE [dbo].[Formularios2](
 ) ON [PRIMARY]
 GO
 
-INSERT INTO [dbo].[Formularios2] ([campo],[valor],[idioma])
-      VALUES('lblTitulo','Formulario de insercción de datos de usuario','es'),
-	 ('gbxInformacionPersonal','Información personal','es'),
-	 ('lblNombre','Nombre','es'),
-	 ('lblApellidos','Apellidos','es'),
-	 ('lblCorreo','Correo','es'),
-	 ('gbxTarjeta','Tarjeta','es'),
-	 ('lblTipoDeTarjeta','Tipo de tarjeta','es'),
-	 ('lblPropietario','Propietario','es'),
-	  ('lblNumeroDeTarjeta','Número de tarjeta','es'),
-	  ('lblCvv2','CVV2','es'),
-	  ('lblFechaDeCaducidad','Fecha de caducidad','es'),
-	  ('gbxDireccionDeFacturacion','Dirección de facturación','es'),
-	  ('lblDireccion','Dirección','es'),
-	  ('lblCiudad','Ciudad','es'),
-	  ('lblComunidad','Comunidad','es'),
-	  ('lblC.P.','C.P.','es'),
-	  ('lblPais','País','es'),
-	  ('lblTelefono','Teléfono','es'),
-	  ('chbxCertifico','Certifico que soy mayor de edad y acepto los términos.','es');
+INSERT INTO [dbo].[Textos] ([campo],[valor],[idioma])
+      VALUES('lblTitulo','Formulario de insercción de datos de usuario','es-ES'),
+	 ('gbxInformacionPersonal','Información personal','es-ES'),
+	 ('lblNombre','Nombre','es-ES'),
+	 ('lblApellidos','Apellidos','es-ES'),
+	 ('lblCorreo','Correo','es-ES'),
+	 ('gbxTarjeta','Tarjeta','es-ES'),
+	 ('lblTipoDeTarjeta','Tipo de tarjeta','es-ES'),
+	 ('lblPropietario','Propietario','es-ES'),
+	  ('lblNumeroDeTarjeta','Número de tarjeta','es-ES'),
+	  ('lblCvv2','CVV2','es-ES'),
+	  ('lblFechaDeCaducidad','Fecha de caducidad','es-ES'),
+	  ('gbxDireccionDeFacturacion','Dirección de facturación','es-ES'),
+	  ('lblDireccion','Dirección','es-ES'),
+	  ('lblCiudad','Ciudad','es-ES'),
+	  ('lblComunidad','Comunidad','es-ES'),
+	  ('lblC.P.','C.P.','es-ES'),
+	  ('lblPais','País','es-ES'),
+	  ('lblTelefono','Teléfono','es-ES'),
+	  ('chbxCertifico','Certifico que soy mayor de edad y acepto los términos.','es-ES');
 GO
 
-INSERT INTO [dbo].[Formularios2] ([campo],[valor],[idioma])
-	 VALUES('lblTitulo','Please complete the form below','en'),
-	  ('gbxInformacionPersonal','Personal information','en'),
-	  ('lblNombre','First name','en'),
-	  ('lblApellidos','Last name','en'),
-	  ('lblCorreo','Email','en'),
-	  ('gbxTarjeta','Credit card information','en'),
-	  ('lblTipoDeTarjeta','Credit card type','en'),
-	  ('lblPropietario','Card holder','en'),
-	  ('lblNumeroDeTarjeta','Card number','en'),
-	  ('lblCvv2','CVV2','en'),
-	  ('lblFechaDeCaducidad','Exp. date','en'),
-	  ('gbxDireccionDeFacturacion','Billing address','en'),
-	  ('lblDireccion','Street address','en'),
-	  ('lblCiudad','City','en'),
-	  ('lblComunidad','State','en'),
-	  ('lblC.P.','ZIP','en'),
-	  ('lblPais','County','en'),
-	  ('lblTelefono','Phone','en'),
-	  ('chbxCertifico','Check here to certify that you are 18 years of age or older and agree to the terms this purchase.','en');
+INSERT INTO [dbo].[Textos] ([campo],[valor],[idioma])
+	 VALUES('lblTitulo','Please complete the form below','en-EN'),
+	  ('gbxInformacionPersonal','Personal information','en-EN'),
+	  ('lblNombre','First name','en-EN'),
+	  ('lblApellidos','Last name','en-EN'),
+	  ('lblCorreo','Email','en-EN'),
+	  ('gbxTarjeta','Credit card information','en-EN'),
+	  ('lblTipoDeTarjeta','Credit card type','en-EN'),
+	  ('lblPropietario','Card holder','en-EN'),
+	  ('lblNumeroDeTarjeta','Card number','en-EN'),
+	  ('lblCvv2','CVV2','en-EN'),
+	  ('lblFechaDeCaducidad','Exp. date','en-EN'),
+	  ('gbxDireccionDeFacturacion','Billing address','en-EN'),
+	  ('lblDireccion','Street address','en-EN'),
+	  ('lblCiudad','City','en-EN'),
+	  ('lblComunidad','State','en-EN'),
+	  ('lblC.P.','ZIP','en-EN'),
+	  ('lblPais','County','en-EN'),
+	  ('lblTelefono','Phone','en-EN'),
+	  ('chbxCertifico','Check here to certify that you are 18 years of age or older and agree to the terms this purchase.','en-EN');
